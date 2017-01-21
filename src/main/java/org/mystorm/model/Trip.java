a99edd02110d6f8a030a5e8aa91fbf6ea0238c71
Alberto Corral <acorralortiz@gmail.com>
2017-01-22 00:49:47 +0100

    added support for lombok

--- src/main/java/org/mystorm/model/Trip.java
+++ src/main/java/org/mystorm/model/Trip.java
@@ -7,7 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Trip {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
