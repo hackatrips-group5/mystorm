3d7fa905f3ae43c4152917a486252a06180a37a7
Alberto Corral <acorralortiz@gmail.com>
2017-01-21 23:58:22 +0100

    rebase

---
+++ src/main/java/org/mystorm/service/ApiServices.java
@@ -0,0 +1,17 @@
+package org.mystorm.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Aggregate all API services
+ * - minube
+ * - others...
+ * 
+ * @author alberto
+ *
+ */
+
+@Service
+public class ApiServices {
+	
+}
---
+++ src/main/java/org/mystorm/service/TripServices.java
@@ -0,0 +1,16 @@
+package org.mystorm.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Aggregate all Trips services and repositories
+ * 
+ * 
+ * @author alberto
+ *
+ */
+
+@Service
+public class TripServices {
+
+}
